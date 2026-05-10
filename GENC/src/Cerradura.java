18ea66314cd6d17f4b8aefd0b97668a146a785b6
Windbaires <Windbaires@Windbaires-PC>
2026-05-10 19:17:18 -0300

    Clase Cerradura

---
+++ GENC/src/Cerradura.java
@@ -0,0 +1,198 @@
+
+// 9.	Implementar la clase Cerradura con los siguientes métodos. Indique axiomas de la clase, pre y post condiciones de los métodos.
+// Cuando una Cerradura se bloquea no puede volver a abrirse nunca más. 
+
+/** Estos deben cumplirse siempre (Axiomas):
+
+La clave es válida:
+0 ≤ claveDeApertura ≤ 9999
+La cantidad de fallos consecutivos para bloquear es válida:
+1 ≤ cantidadDeFallosConsecutivosQueLaBloquean ≤ 5
+Consistencia de estados:
+estaAbierta ≠ estaCerrada
+Estado inicial:
+estaAbierta = false
+estaCerrada = true
+bloqueada = false
+contadorDeFallosConsecutivos ≥ 0
+contarAperturasExitosas ≥ 0
+contarAperturasFallidas ≥ 0
+Irreversibilidad del bloqueo:
+bloqueada entonces siempre bloqueada
+(una vez true, nunca vuelve a false)
+**/
+
+public class Cerradura {
+	
+	private int claveDeApertura;
+	private int cantidadDeFallosConsecutivosQueLaBloquean;
+	private boolean estaAbierta;
+	private boolean estaCerrada;
+	private int contadorDeFallosConsecutivos;
+	private boolean bloqueada;
+	private int contarAperturasExitosas;
+	private int contarAperturasFallidas;
+	
+	/**Pre: claveDeApertura  hasta 4 digitos mayores o iguales que cero y menores 10000.
+	 *      la cantidad de fallos consecutivos es numero mayor a cero y menor a 6.
+	 *      La cerradura se inicializa cerrada.
+	 * Post:
+	 * estaAbierta = false
+     * estaCerrada = true
+     * bloqueada = false
+     * contadorDeFallosConsecutivos = 0
+     * contarAperturasExitosas = 0
+     *  contarAperturasFallidas = 0
+	 *      
+	 * @param claveDeApertura
+	 * @param cantidadDeFallosConsecutivosQueLaBloquean
+	 */
+
+    public Cerradura(int claveDeApertura,
+                     int cantidadDeFallosConsecutivosQueLaBloquean) {
+    	validarClave(claveDeApertura);
+    	
+    	if (cantidadDeFallosConsecutivosQueLaBloquean >= 6 || cantidadDeFallosConsecutivosQueLaBloquean <= 0) {
+    		throw new IllegalArgumentException("la cantidad de fallos consecutivos es numero mayor a cero y menor a 6");
+    	}
+    	this.claveDeApertura = claveDeApertura;
+    	this.cantidadDeFallosConsecutivosQueLaBloquean = cantidadDeFallosConsecutivosQueLaBloquean;
+    	this.estaAbierta = false;
+    	this.estaCerrada = true;
+    	this.bloqueada = false;
+    	this.contadorDeFallosConsecutivos = 0;
+    	this.contarAperturasExitosas = 0;
+    	this.contarAperturasFallidas = 0;
+    	
+    	
+    }
+    
+    private void validarClave(int clave) {
+    	if (clave > 9999 || clave < 0) {
+    		throw new IllegalArgumentException("hasta 4 digitos mayores o iguales que cero y menores 10000.");
+    	}
+    }
+    /**
+     * pre: La cerradura no debe estar bloqueada
+     * La cerradura no debe estar abierta
+     * 0 ≤ clave ≤ 9999
+     * 
+     * post: 
+     * Si la clave es correcta:
+     * estaAbierta = true
+     * estaCerrada = false
+     * contadorDeFallosConsecutivos = 0
+     * contarAperturasExitosas++
+     * 
+     * Si la clave es incorrecta:
+     * estaAbierta no cambia
+     * estaCerrada no cambia
+     * contadorDeFallosConsecutivos++
+     * contarAperturasFallidas++
+     * 
+     * Si se alcanza el límite:
+     * bloqueada = true
+     * 
+     * @param clave
+     * @return
+     */
+    
+
+    public boolean abrir(int clave) {
+    	
+    	if (bloqueada) {
+    		throw new IllegalStateException("La cerradura esta bloqueada");   		
+    	}
+    	if (estaAbierta) {
+    		throw new IllegalStateException("La cerradura ya esta abierta");
+    	}
+    	validarClave(clave);
+    	if (claveDeApertura != clave) {
+    		contadorDeFallosConsecutivos++;
+    		contarAperturasFallidas++;
+    		return false;
+    	}
+    	
+    	contarAperturasExitosas++;
+    	contadorDeFallosConsecutivos = 0;
+    	estaAbierta = true;
+    	estaCerrada = false;
+    		return true;   	 
+    	    	
+    }
+    /**
+     * pre: La cerradura no debe estar bloqueada
+     * La cerradura debe estar abierta (estaAbierta = true)
+     * 
+     * post: estaAbierta = false
+     * estaCerrada = true
+     */
+
+    public void cerrar() {
+    	if (bloqueada) {
+    		throw new IllegalStateException("La cerradura fue bloqueda");
+    	}
+    	
+    	if (estaCerrada) {
+    		throw new IllegalStateException("La cerradura ya esta cerrada");
+    	}
+    	estaAbierta = false;
+    	estaCerrada = true;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+
+    public boolean estaAbierta() {
+    	return estaAbierta;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+
+    public boolean estaCerrada() {
+    	return estaCerrada;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+
+    public boolean fueBloqueada() {
+    	if (bloqueada) {
+    		throw new IllegalStateException("La cerradura fue bloqueada");
+    	}
+    	if (cantidadDeFallosConsecutivosQueLaBloquean == contadorDeFallosConsecutivos) {
+    		bloqueada = true;
+    	}
+    	
+    	return bloqueada;
+    		
+    	
+    }
+    
+    /**
+     * 
+     * @return
+     */
+
+    public int contarAperturasExitosas() {
+    	return contarAperturasExitosas;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+
+    public int contarAperturasFallidas() {
+    	return contarAperturasFallidas;
+    }
+
+}
+
